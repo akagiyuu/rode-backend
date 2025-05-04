6e9c3e0f032ebe8305f3281525f29176ea784fe0
akagiyuu <khanghuynh0245@gmail.com>
2025-05-04 13:09:28 +0700

    feat(html-judge): add config

---
+++ judges/html-css-judge/src/config.rs
@@ -0,0 +1,32 @@
+use std::sync::LazyLock;
+
+use serde::Deserialize;
+
+fn default_amqp_url() -> String {
+    String::from("amqp://127.0.0.1:5672/%2f")
+}
+
+#[derive(Deserialize)]
+pub struct Config {
+    #[serde(default = "default_amqp_url")]
+    pub amqp_url: String,
+
+    pub id: String,
+
+    pub html_queue_name: String,
+
+    pub database_url: String,
+
+    pub width: u32,
+
+    pub height: u32
+}
+
+pub static CONFIG: LazyLock<Config> = LazyLock::new(|| {
+    ::config::Config::builder()
+        .add_source(::config::Environment::default().try_parsing(true))
+        .build()
+        .unwrap()
+        .try_deserialize()
+        .unwrap()
+});
